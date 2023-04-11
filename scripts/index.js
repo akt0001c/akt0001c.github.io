26d17a9ad379ecf8b6d42299efa6372c1f640223
akt0001c <110126989+akt0001c@users.noreply.github.com>
2023-04-11 17:46:44 +0530

    update

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,33 @@
-let downloadbtn= document.querySelectorAll(".resume-btn");
+let downloadbtn= document.querySelectorAll(".resume");
 for(let i=0;i<downloadbtn.length;i++)
 {
     downloadbtn[i].onclick= ()=>{
         open("https://drive.google.com/file/d/1m76iN3TilPN4damYcp9Y-y8HTtEXSoMO/view?usp=share_link");
     };
+}
+
+let opentbtn= document.querySelector("#option");
+let closebtn= document.querySelector("#close-btn");
+
+closebtn.onclick= ()=>{
+    document.querySelector("#side-menu").style.width="0%";
+}
+
+opentbtn.onclick= (e)=>{
+    e.preventDefault();
+    document.querySelector("#side-menu").style.width="50%";
+    console.log("open working");
+};
+
+
+
+
+//stop the auto refresh feature of side navaber
+
+let sideoptions= document.querySelectorAll(".side-menu-options");
+for(let i=0;i<sideoptions.length;i++)
+{
+     sideoptions[i].onclick= (e)=>{
+        e.preventDefault();
+     }
 }
