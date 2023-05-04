6cada3571c2d50d0a9a57d1eecf0f2a3a3275462
akt0001c <110126989+akt0001c@users.noreply.github.com>
2023-05-04 08:58:50 +0530

    update

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,15 @@
 let downloadbtn= document.querySelectorAll(".resume");
- 
+let githubLink= ["https://github.com/akt0001c/fumbling-hand-3558.git",
+                 "https://github.com/akt0001c/reach-wren-1563.git",
+                 "https://github.com/akt0001c/brash-profit-9147.git",
+                 "https://github.com/akt0001c/subdued-money-2515.git"];
+
+let deployORVideoLink=["https://aesthetic-capybara-e0c08f.netlify.app/",
+                       "https://dynamic-dolphin-4e9832.netlify.app/",
+                       "https://drive.google.com/file/d/1tWH0XZIBY5bSUa8G6E_m8N9hI-UczItT/view?usp=sharing",
+                       "https://drive.google.com/file/d/1L09UbTLdAD9NPXcjDx1MDzVznJne-55u/view?usp=sharing"];
+
+
 for(let i=0;i<downloadbtn.length;i++)
 {
     downloadbtn[i].onclick= (event)=>{
@@ -14,6 +24,9 @@ for(let i=0;i<downloadbtn.length;i++)
 
 let opentbtn= document.querySelector("#option");
 let closebtn= document.querySelector("#close-btn");
+let githubBtn= document.querySelectorAll(".project-github-link");
+let deployBtn= document.querySelectorAll(".project-deployed-link");
+
 
 closebtn.onclick= ()=>{
     document.querySelector("#side-menu").style.width="0%";
@@ -25,8 +38,23 @@ opentbtn.onclick= (e)=>{
     console.log("open working");
 };
 
+for(let a=0;a<githubBtn.length;a++)
+ {
+    githubBtn[a].onclick= ()=>{
+        open(githubLink[a]);
+    };
+ }
 
+ for(let b=0; b<deployBtn.length;b++)
+  {
+    deployBtn[b].onclick= ()=>{
+        open(deployORVideoLink[b]);
+    }
+  }
 
+ 
+
+let locationarr=["#skills-section","#project-section","","#main"];  
 
 //stop the auto refresh feature of side navaber
 
@@ -35,5 +63,7 @@ for(let i=0;i<sideoptions.length;i++)
 {
      sideoptions[i].onclick= (e)=>{
         e.preventDefault();
+        document.querySelector("#side-menu").style.width="0%";
+        window.location.hash=locationarr[i];
      }
 }
